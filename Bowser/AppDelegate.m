0eb5a05fbf031ff5bcc3e4bfc7c7b66d006bcd8a
Colin Roth <colin@getwoozy.com>
2015-04-17 18:57:46 -0400

    Functional web browser

--- Bowser/AppDelegate.m
+++ Bowser/AppDelegate.m
@@ -7,9 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
-
+@property (nonatomic, strong) ViewController *viewcontroller;
 @end
 
 @implementation AppDelegate
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
