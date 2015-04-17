125eb088a6ff4408aabe664751cadff1a0ca3815
Colin Roth <colin@getwoozy.com>
2015-04-17 07:07:18 -0400

    Initial Commit

---
+++ Bowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bowser
+//
+//  Created by Colin M. Roth on 4/17/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bowser
+//
+//  Created by Colin M. Roth on 4/17/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
